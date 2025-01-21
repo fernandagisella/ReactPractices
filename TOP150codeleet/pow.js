f80dd19c173a6f26b1e5f554b78aa85d2c88d038
Fernanda Gisella Martínez Pichardo <fernandagisella.pichardo@gmail.com>
2025-01-20 23:26:06 -0600

    Pow(x,n)

---
+++ TOP150codeleet/pow.js
@@ -0,0 +1,9 @@
+/**
+ * Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    return Math.pow(x, n)
+};
